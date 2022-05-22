7c9aca1b76f36ca03dcfac49d044ace4cae32d60
Janice Pena <jipena94@gmail.com>
2022-05-21 17:08:27 -0700

    Workshop Week 2 Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -4,6 +4,41 @@ const Campsite = require("../models/campsite");
 const campsiteRouter = express.Router();
 
 campsiteRouter
+  .route("/")
+  .get((req, res, next) => {
+    Campsite.find()
+      .then((campsites) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsites);
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Campsite.create(req.body)
+      .then((campsite) => {
+        console.log("Campsite Created ", campsite);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(campsite);
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /campsites");
+  })
+  .delete((req, res, next) => {
+    Campsite.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  });
+
+campsiteRouter
   .route("/:campsiteId/comments")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)

c78f6acd57a5ce0f88d32e172e74b07330583ddd
Janice Pena <jipena94@gmail.com>
2022-05-27 14:55:50 -0700

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,12 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.plugin(passportLocalMongoose);

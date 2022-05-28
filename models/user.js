87481fe7d73dc941d34fd629387bdb9f9c4db924
Janice Pena <jipena94@gmail.com>
2022-05-28 16:50:05 -0700

    Workshop Assignment Week 3

--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firstname: {
+    type: String,
+    default: "",
+  },
+  lastname: {
+    type: String,
+    default: "",
+  },
   admin: {
     type: Boolean,
     default: false,
@@ -10,3 +18,5 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);

837da248d1ded92aae24eb0598d074a3bd0fb3be
Janice Pena <jipena94@gmail.com>
2022-06-01 17:39:58 -0700

    Passport Facebook

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ app.use("/partners", partnerRouter);
 app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
   next(createError(404));
-});
+});*/
 
 // error handler
 app.use(function (err, req, res, next) {
